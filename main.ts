d7278e512927cabb16e47a25766e299a5f66a289
Karthikeyan Vaithilingam <seenukarthi@gmail.com>
2023-12-26 20:52:15 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.StickFigure)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.forever(function () {
+	
+})
